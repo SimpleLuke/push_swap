b121759daf3ece136affe4c953038da437b25459
Luke Lai <llai@c1r1s7.42london.com>
2023-12-14 13:16:34 +0000

    Add stack_size prototype and doubly linked list struct

--- push_swap.c
+++ push_swap.c
@@ -6,13 +6,32 @@
 /*   By: llai <llai@student.42london.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 13:28:38 by llai              #+#    #+#             */
-/*   Updated: 2023/12/13 13:42:56 by llai             ###   ########.fr       */
+/*   Updated: 2023/12/14 11:35:31 by llai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/* ************************************************************************** 
+ *  Summary of File:                                                          
+ *  
+ *  	This program contains code which sorts a stack of integers.
+ *  	The program performs sorting by the following instructions:
+ *  	- sa(swap a), sb(swap b), ss(sa & sb)
+ *  	- pa(push a), pb(push b)
+ *  	- ra(rotate a), rb(rotate b), rr(ra & rb)
+ *  	- rra(reverse rotate a), rrb(reverse rotate b), rrr(rra & rrb)
+ *  	The program displays the smallest list of instructions possible
+ *  	to sort the stack.
+ *
+ * ************************************************************************** */
+#include "push_swap.h"
+
 int	main(int argc, char **argv)
 {
+	int	size;
+
 	(void)argv;
 	if (argc < 2)
 		return (0);
+	size = stack_size(argv);
+	
 }
--- push_swap.h
+++ push_swap.h
@@ -6,14 +6,35 @@
 /*   By: llai <llai@student.42london.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 13:35:00 by llai              #+#    #+#             */
-/*   Updated: 2023/12/13 13:36:03 by llai             ###   ########.fr       */
+/*   Updated: 2023/12/14 13:16:04 by llai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
-#def PUSH_SWAP_H
+# define PUSH_SWAP_H
 
-#include <unistd.h>
+# include <unistd.h>
+# include <stdlib.h>
 
+typedef struct s_node
+{
+	int				value;
+	struct node_s	*prev;
+	struct node_s	*next;
+}	t_node;
+
+/* **************************************************************************
+ * size_t stack_size(char **argv)
+ *
+ * Summary of the function:
+ * 
+ * 	This function finds the size of the input integers,
+ * 	it helps to manipulate the stacks/lists.
+ *
+ * Parameters : array of string - index 0 would be the programme name.
+ *
+ * Return Value : Positive integer as the size.
+ * **************************************************************************/
+size_t	stack_size(char **argv);
 
 #endif
