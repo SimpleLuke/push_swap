8897dd0733d1d4bcac062b500b17605ab63aebb1
Luke Lai <llai@c1r1s7.42london.com>
2023-12-14 14:18:14 +0000

    Add prototypes for checking errors

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: llai <llai@student.42london.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 13:28:38 by llai              #+#    #+#             */
-/*   Updated: 2023/12/14 11:35:31 by llai             ###   ########.fr       */
+/*   Updated: 2023/12/14 14:17:45 by llai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,13 @@
 
 int	main(int argc, char **argv)
 {
-	int	size;
+	int		size;
+	t_node	*first;
 
-	(void)argv;
 	if (argc < 2)
-		return (0);
-	size = stack_size(argv);
-	
+		return (-1);
+	check_valid_arg(argv + 1);
+	size = stack_size(argv + 1);
+	first = init_list(argv + 1, size);
+	push_swap(first, size);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: llai <llai@student.42london.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 13:35:00 by llai              #+#    #+#             */
-/*   Updated: 2023/12/14 13:16:04 by llai             ###   ########.fr       */
+/*   Updated: 2023/12/14 14:11:44 by llai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,22 @@ typedef struct s_node
 }	t_node;
 
 /* **************************************************************************
- * size_t stack_size(char **argv)
+ * void	check_valid_arg(char **int_str)
+ *
+ * Summary of the function:
+ * 
+ * 	This function checks if the arguments are valid,
+ * 	which they are NOT non numeric, duplicated and within the value of int.
+ * 	In invalid cases, it outputs "Error" and exit the program.
+ *
+ * Parameters : A pointer to the argument string.
+ *
+ * Return Value : It returns nothing.
+ * **************************************************************************/
+void	check_valid_arg(char **int_str);
+
+/* **************************************************************************
+ * size_t stack_size(char **int_str)
  *
  * Summary of the function:
  * 
@@ -35,6 +50,51 @@ typedef struct s_node
  *
  * Return Value : Positive integer as the size.
  * **************************************************************************/
-size_t	stack_size(char **argv);
+size_t	stack_size(char **int_str);
+
+/* **************************************************************************
+ * t_node	*init_list(char **int_str, size_t size)
+ *
+ * Summary of the function:
+ * 
+ * 	This function initialises the list from arguments.
+ * 	It sets up a doubly linked list.
+ *
+ * Parameters : The arguments integers from the terminal 
+ * 				(index 0 is programme name) and the size of the list.
+ *
+ * Return Value : It returns the first node of the list.
+ * **************************************************************************/
+t_node	*init_list(char **int_str, size_t size);
+
+/* **************************************************************************
+ * t_node	*add_to_list(int value, t_node *prev, t_node *next)
+ *
+ * Summary of the function:
+ * 
+ * 	This function initialises the node in the doubly linked list.
+ * 	It sets up the value and points to previous and next node.
+ *
+ * Parameters : The value is an integer. prev and node are pointing to
+ * 				a node struct.
+ *
+ * Return Value : It returns a new node.
+ * **************************************************************************/
+t_node	*add_to_list(int value, t_node *prev, t_node *next);
+
+/* **************************************************************************
+ * void	push_swap(t_node *first, size_t size)
+ *
+ * Summary of the function:
+ * 
+ * 	This function sorts the list and ouput the steps.
+ * 	It excutes different sorting algorithms depends on the size
+ * 	of the list.
+ *
+ * Parameters : The first node of the list and the size of the list
+ *
+ * Return Value : It returns nothing.
+ * **************************************************************************/
+void	push_swap(t_node *first, size_t size);
 
 #endif
